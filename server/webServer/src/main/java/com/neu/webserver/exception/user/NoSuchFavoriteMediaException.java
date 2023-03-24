58d537c609f8c8601cb921095a5d1d0966f818fa
haifeipan <haifeipan1006@gmail.com>
2023-03-23 21:07:34 -0400

    added NoSuchFavouriteMediaException

---
+++ server/webServer/src/main/java/com/neu/webserver/exception/user/NoSuchFavoriteMediaException.java
@@ -0,0 +1,8 @@
+package com.neu.webserver.exception.user;
+
+public class NoSuchFavoriteMediaException extends RuntimeException {
+
+    public NoSuchFavoriteMediaException(String message) {
+        super(message);
+    }
+}
