20663026cce91538aa10430026b7ee75a4ea4d2d
chen <fancle666@gmail.com>
2023-03-25 21:10:42 -0400

    android client setup

---
+++ client/android/app/src/main/java/edu/northeastern/ease_music_andriod/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.ease_music_andriod;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
