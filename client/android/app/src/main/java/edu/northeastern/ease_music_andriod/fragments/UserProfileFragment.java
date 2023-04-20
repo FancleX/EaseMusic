d5cbc013fa370f04bd7f6b1ea28fac4c3fad73b3
chen <fancle666@gmail.com>
2023-04-19 20:08:53 -0400

    added camera support

--- client/android/app/src/main/java/edu/northeastern/ease_music_andriod/fragments/UserProfileFragment.java
+++ client/android/app/src/main/java/edu/northeastern/ease_music_andriod/fragments/UserProfileFragment.java
@@ -1,15 +1,24 @@
 package edu.northeastern.ease_music_andriod.fragments;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,17 +29,21 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Objects;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import edu.northeastern.ease_music_andriod.R;
-import edu.northeastern.ease_music_andriod.recyclerViewComponents.musicItem.MusicItemAdapter;
 import edu.northeastern.ease_music_andriod.recyclerViewComponents.userFavoriteItem.FavoriteItemAdapter;
 import edu.northeastern.ease_music_andriod.utils.DataCache;
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 public class UserProfileFragment extends Fragment {
 
@@ -38,6 +51,10 @@ public class UserProfileFragment extends Fragment {
     private final DataCache dataCache = DataCache.getInstance();
     private static final String AUDIO_DIR = "AUDIOS";
     private FavoriteItemAdapter favoriteItemAdapter;
+    private ActivityResultLauncher<String> requestCameraPermissionLauncher;
+    private ActivityResultLauncher<Uri> takePictureLauncher;
+    private ActivityResultLauncher<String> pickImageLauncher;
+
 
     // ================ views ================
     private Button signOutButton;
@@ -89,8 +106,111 @@ public class UserProfileFragment extends Fragment {
         favoriteItemAdapter.updateData();
 
         cardView.setOnClickListener(v -> favoriteRecycler.setVisibility(View.VISIBLE));
+
+        profileImage.setOnClickListener(v -> changeProfileImage());
+
+        // Request camera permission
+        requestCameraPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Permission granted, start camera
+                takePictureLauncher.launch(createImageFileUri());
+            } else {
+                // Permission denied
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Take picture from camera
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.TakePicture(), isTaken -> {
+            if (isTaken) {
+                // Query the latest image file from MediaStore
+                String[] projection = { MediaStore.Images.ImageColumns.DATA };
+                String selection = MediaStore.Images.ImageColumns.DATE_TAKEN + "=?";
+                String[] selectionArgs = { String.valueOf(System.currentTimeMillis()) };
+                String sortOrder = MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC";
+                Cursor cursor = requireActivity().getContentResolver().query(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        sortOrder
+                );
+
+                // Get the image file path
+                String imagePath = null;
+                if (cursor != null && cursor.moveToFirst()) {
+                    imagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA));
+                    cursor.close();
+                }
+
+                // Display the image
+                if (imagePath != null) {
+                    Picasso.get()
+                            .load(new File(imagePath))
+                            .resize(100, 100)
+                            .transform(new CropCircleTransformation())
+                            .into(profileImage);
+                }
+            }
+        });
+
+        // Pick image from gallery
+        pickImageLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), result -> {
+            if (result != null) {
+                Picasso.get()
+                        .load(result)
+                        .resize(100, 100)
+                        .transform(new CropCircleTransformation())
+                        .into(profileImage);
+            }
+        });
+
+
         return root;
     }
+
+    private void changeProfileImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Choose an option");
+        builder.setItems(options, (dialog, which) -> {
+            if (options[which].equals("Take Photo")) {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, start camera
+                    takePictureLauncher.launch(createImageFileUri());
+                } else {
+                    // Request camera permission
+                    requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+                }
+            } else if (options[which].equals("Choose from Gallery")) {
+                pickImageLauncher.launch("image/*");
+            } else if (options[which].equals("Cancel")) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    private Uri createImageFileUri() {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile;
+
+        try {
+            // Create the image file in the external storage directory
+            imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+            // Get the content URI for the image file using FileProvider
+            return FileProvider.getUriForFile(requireContext(), requireActivity().getPackageName() + ".fileprovider", imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 
     private int getDownloadCount() {
         File dir = new File(requireContext().getExternalFilesDir(null), AUDIO_DIR);
@@ -101,8 +221,6 @@ public class UserProfileFragment extends Fragment {
 
         return 0;
     }
-
-
 
     private void switchToLoginPage() {
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
